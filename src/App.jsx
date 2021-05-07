00324663e1616f69dbed5524afaf35754bb66579
David Röger <david@codeatelier.com>
2021-05-07 18:09:19 +0200

    Update template

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,32 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen flex-col">
-      <h1 className="text-5xl mb-[1rem]">Welcome to the React + Tailwind + Vite Starter</h1>
-      <p>Made by Haardik Gupta</p>
-    </div>
+    <section className="container p-4">
+      <h1 className="text-8xl my-8">
+        Development System
+      </h1>
+      <ul className="p-8 bg-gradient-to-r border-2 border-black bg-opacity-20">
+        <li className="list-disc list-inside text-xl">
+          Build Components
+        </li>
+        <li className="list-disc list-inside text-xl">
+          Compare Componets
+        </li>
+        <li className="list-disc list-inside text-xl">
+          Test Componts
+        </li>
+        <li className="list-disc list-inside text-xl">
+          Distribute Components
+        </li>
+        <li className="list-disc list-inside text-xl">
+          Single Source of Truth
+        </li>
+      </ul>
+      <p>	
+        <br/>
+        Build using React and Storybook :(
+      </p>
+    </section>
   )
 }
 
