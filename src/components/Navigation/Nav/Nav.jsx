077e1cd088fe25ae320482718faae70b05f66dcd
David Röger <david@codeatelier.com>
2021-06-03 03:39:20 +0200

    238aa208a0460cb435b1cdf715fd5337ef883342

--- src/components/Navigation/Nav/Nav.jsx
+++ src/components/Navigation/Nav/Nav.jsx
@@ -2,40 +2,19 @@
 
 import React from 'react';
 import {NavIconButton, NavIcon, Text} from '../../index'
+import { navItems } from './Nav.config'
 
 export function Nav() {
-    return <nav className="bg-black w-full md:grid md:grid-cols-md md:gap-md lg:block lg:h-full lg:col-start-1 xl:col-span-2">
+    return <nav className="bg-black w-full md:grid md:grid-cols-md md:gap-md lg:block lg:py-md lg:h-full lg:col-start-1 xl:col-span-2">
         <ul className="flex w-full justify-between md:col-start-2 md:col-span-2 lg:flex-col">
-            <li>
-                <NavIconButton>
-                    <NavIcon></NavIcon>
-                    <Text colorClass="white">Home</Text>
-                </NavIconButton>
-            </li>
-            <li>
-                <NavIconButton>
-                    <NavIcon type="devices"></NavIcon>
-                    <Text colorClass="white">Geräte</Text>
-                </NavIconButton>
-            </li>
-            <li>
-                <NavIconButton active>
-                    <NavIcon type="rooms"></NavIcon>
-                    <Text colorClass="white">Räume</Text>
-                </NavIconButton>
-            </li>
-            <li>
-                <NavIconButton>
-                    <NavIcon type="automations"></NavIcon>
-                    <Text colorClass="white">Szenen</Text>
-                </NavIconButton>
-            </li>
-            <li>
-                <NavIconButton>
-                    <NavIcon type="homee"></NavIcon>
-                    <Text colorClass="white">homee</Text>
-                </NavIconButton>
-            </li>
+            {navItems.map((navItem, index) => (
+                <li className="lg:mb-sm lg:last:mb-0" key={index}>
+                    <NavIconButton>
+                        <NavIcon type={navItem.type}></NavIcon>
+                        <Text colorClass="white">{navItem.text}</Text>
+                    </NavIconButton>
+                </li>
+            ))}
         </ul>
     </nav>
 };
