74e9e4c4e213219adbe1a68cbec54ddcfdab8da3
David Röger <david@codeatelier.com>
2021-06-03 03:06:03 +0200

    Adapt button handling for text

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,8 +5,8 @@ import { Text } from '../index'
 export function Button({children, handleClick}) {
     
     return (
-    <button onClick={handleClick} className={`m-sm px-md py-xs rounded-sm bg-black text-white hover:bg-gray-500 active:bg-gray-700 border border-black focus:outline-none ring-width-0.5 ring-offset-2 ring-black focus:ring`}>
-        <Text>{children}</Text>
+    <button onClick={handleClick} className={`m-sm px-md py-xs rounded-sm bg-black hover:bg-gray-500 active:bg-gray-700 border border-black focus:outline-none ring-width-0.5 ring-offset-2 ring-black focus:ring`}>
+        <Text colorClass="white">{children}</Text>
     </button>)
 };
 
