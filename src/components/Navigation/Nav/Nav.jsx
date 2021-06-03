3620c28454c85ec4f670258ca11812c987e5a410
David Röger <david@codeatelier.com>
2021-06-03 03:14:10 +0200

    Make gird responsive

--- src/components/Navigation/Nav/Nav.jsx
+++ src/components/Navigation/Nav/Nav.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 import {NavIconButton, NavIcon, Text} from '../../index'
 
 export function Nav() {
-    return <nav className="bg-black w-full md:grid md:grid-cols-md">
-        <ul className="flex w-full justify-between md:col-start-2 md:col-span-2">
+    return <nav className="bg-black w-full md:grid md:grid-cols-md md:gap-md lg:block lg:h-full lg:col-start-1 xl:col-span-2">
+        <ul className="flex w-full justify-between md:col-start-2 md:col-span-2 lg:flex-col">
             <li>
                 <NavIconButton>
                     <NavIcon></NavIcon>
