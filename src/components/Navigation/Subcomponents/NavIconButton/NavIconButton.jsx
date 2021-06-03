238aa208a0460cb435b1cdf715fd5337ef883342
David Röger <david@codeatelier.com>
2021-06-03 03:39:06 +0200

    Fix icon shrink + external nav config
    closes #3

--- src/components/Navigation/Subcomponents/NavIconButton/NavIconButton.jsx
+++ src/components/Navigation/Subcomponents/NavIconButton/NavIconButton.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Text, NavIcon } from '../../../index'
 
 export function NavIconButton({ active, target, children }) {
-    return <a href={target} className="bg-black hover:bg-gray-500 active:bg-gray-700 p-xs md:pr-sm lg:p-sm lg:pr-md block h-auto select-none">
-        <div className="flex">
-            <div className="p-xs w-fit-content inline-block md:flex md:flex-row-reverse md:items-center">
+    return <a href={target} className="bg-black hover:bg-gray-500 active:bg-gray-700 p-xs lg:pr-sm lg:p-sm lg:pr-lg block h-auto select-none">
+        <div className="flex align-center">
+            <div className="p-xs w-fit-content inline-block lg:flex lg:flex-row-reverse lg:items-center">
                 {children[0]}
-                <div className="w-xs h-xs mt-xs mx-auto md:my-auto md:mr-xs lg:mr-sm">
+                <div className="w-xs h-xs mt-xs mx-auto lg:my-auto lg:mr-xs lg:mr-sm">
                     {
                         active && <div className="bg-white w-full h-full rounded-full"></div>
                     }
                 </div>
             </div>
-            <p className="hidden md:inline-block md:ml-xs lg:ml-sm">
+            <p className="hidden lg:block lg:ml-xs xl:ml-sm">
                 {children[1]}
             </p>
         </div>
