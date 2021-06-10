a2c54b6b3cb8c751e2e3fc7a734ece702e68265f
David Röger <david@codeatelier.com>
2021-06-11 00:55:57 +0200

    Purge

--- library/components/ControlElements/Toggle/SubComponents/ToggleScala/ToggleScala.config.js
+++
@@ -1,3 +0,0 @@
-export let scalas = {
-    onOff: ["Aus", "An"],
-};
--- library/components/Text/Text.config.js
+++
@@ -1,6 +0,0 @@
-export let colorClasses = {
-    'black': 'text-black',
-    'gray-700': 'text-gray-700',
-    'gray-500': 'text-gray-500',
-    'white': 'text-white',
-}
