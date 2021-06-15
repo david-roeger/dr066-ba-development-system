3f375f33c598569d4dffe861624937416febca9a
David Röger <david@codeatelier.com>
2021-06-15 18:51:13 +0200

    Update card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -14,26 +14,16 @@ export function Card({title, children, col}) {
     let others = elements.filter(ele => ele.type.name !== 'VideoElement');
 
     let rows = [];
-    others.forEach(((element, index) => {
-        if(!col) {
-            rows[index] = { 
-                value: [element]
-            }
-            return
-        }
-        let newIndex = Math.floor(index / 3);
-        if(!rows[newIndex]) {
-            rows[newIndex] = { 
-                value: []
-            }
-        }
-        rows[newIndex].value.push(element)
-    }));
+    others.forEach(element => {
+        rows.push({ 
+            value: element
+        });
+    });
 
     videos.forEach(element => {
         rows.push({ 
             type: 'VideoElement',
-            value: [element]
+            value: element
         });
         return
     });
@@ -41,23 +31,21 @@ export function Card({title, children, col}) {
 
     return (
         <div className="flex flex-col">
-            <div className="border border-black rounded-t-md bg-black py-sm px-md">
-            <Text size="sm" colorClass="white">
-                { title }
-            </Text>
+            <div className="border border-black rounded-t-md bg-black py-sm px-md lg:p-md lg:px-lg">
+                <Text size="sm" colorClass="white">
+                    { title }
+                </Text>
             </div>
-            <div className="flex flex-col gap-md p-md border-l border-r border-b border-black rounded-b-md">
-                { rows.map((row, index) => (
-                    <Grid key={index} cols={col ? 3 : 0}>
-                        { row.value.map((element, i) => (
-                            <div key={i} className={`${col ? 'flex' : ''} ${col && row.type === 'VideoElement' ? 'col-span-3' : ''} `}>
-                                <div className="mx-auto w-fit-content">
-                                    { element }
-                                </div>
+            <div className="p-sm md:p-md lg:p-lg xl:p-xl border-l border-r border-b border-black rounded-b-md">
+                <Grid cols={col ? undefined : 0}>
+                    { rows.map((element, i) => (
+                        <div key={i} className={`${col ? 'flex' : ''} ${col && element.type === 'VideoElement' ? 'col-span-full' : ''} `}>
+                            <div className="mx-auto w-fit-content">
+                                { element.value }
                             </div>
-                        ))}
-                    </Grid>
-                ))}
+                        </div>
+                    ))}
+                </Grid>
             </div>
         </div>
 
