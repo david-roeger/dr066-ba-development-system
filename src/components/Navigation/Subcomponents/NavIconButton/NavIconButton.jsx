7d59556d05168b2744743cc56e4a13b366c5e580
David Röger <david@codeatelier.com>
2021-06-18 16:11:03 +0200

    Update nav icon
    make whole button clickable

--- src/components/Navigation/Subcomponents/NavIconButton/NavIconButton.jsx
+++ src/components/Navigation/Subcomponents/NavIconButton/NavIconButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, NavIcon } from '../../../index'
 
 export function NavIconButton({ active, children }) {
-    return <div className="flex align-center">
+    return <div className="bg-black hover:bg-gray-500 active:bg-gray-700 flex align-center p-xs pt-sm lg:pt-xs lg:pr-sm lg:p-sm lg:pr-lg">
             <div className="p-xs w-fit-content inline-block lg:flex lg:flex-row-reverse lg:items-center">
                 {children[0]}
                 <div className="w-xs h-xs mt-xs mx-auto lg:my-auto lg:mr-xs lg:mr-sm">
