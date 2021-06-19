6a2c86683bda9ac5a73344eb64019c49e9bfe838
David Röger <david@codeatelier.com>
2021-06-19 18:23:44 +0200

    Make nav responsvie

--- src/components/Navigation/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { NavIconButtonWrapper, NavIconButton, NavIcon, Text} from '../../index'
 import { navItems } from './Navigation.config'
 export function Navigation({children}) {    
-    return <nav className="bg-black w-full sticky top-0 col-span-full md:grid md:grid-cols-md md:gap-md lg:block lg:col-span-1 lg:relative lg:py-md lg:h-full lg:col-start-1 xl:col-span-2 z-50">
+    return <nav className="bg-black w-full sticky top-0 col-span-full md:grid md:grid-cols-md md:gap-md lg:block lg:col-span-1 lg:max-h-screen lg:py-md lg:h-full lg:col-start-1 xl:col-span-2 z-50">
         <ul className="flex gap-md justify-center md:justify-around md:col-start-2 md:col-span-2 lg:flex-col">
             {children.map((child, index) => (
                 <li className="lg:mb-sm lg:last:mb-0" key={index}>
